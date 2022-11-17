0ee9eaaedee89fa48dc78b62d6f99f69c777fd6c
e6d1fe <e6d1fe@gmail.com>
2022-11-17 16:58:57 +0900

    added basename to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route element={<Detail />} path="/movie/:id" />
         <Route element={<Home />} path="/" />
